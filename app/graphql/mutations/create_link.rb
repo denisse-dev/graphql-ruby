170c2d81ee9e5579ed78d992a1311124999c4925
Andrea <crypto.andrea@protonmail.ch>
2019-04-12 11:03:20 -0500

    Add initial mutations

---
+++ app/graphql/mutations/create_link.rb
@@ -0,0 +1,17 @@
+module Mutations
+  class CreateLink < BaseMutation
+    # arguments passed to the `resolved` method
+    argument :description, String, required: true
+    argument :url, String, required: true
+
+    # return type from the mutation
+    type Types::LinkType
+
+    def resolve(description: nil, url: nil)
+      Link.create!(
+        description: description,
+        url: url,
+      )
+    end
+  end
+end
