89434abfe21917c06195b6af2f185f8cf0b98711
Andrea <crypto.andrea@protonmail.ch>
2019-04-12 10:58:46 -0500

    Add queries

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,12 @@
 module Types
-  class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+  class QueryType < BaseObject
+    # queries are just represented as fields
+    # `all_links` is automatically camelcased to `allLinks`
+    field :all_links, [LinkType], null: false
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    # this method is invoked, when `all_link` fields is beeing resolved
+    def all_links
+      Link.all
     end
   end
 end
