ccac3c7b324a481cfb1f04d1df471da83f670307
Andrea <crypto.andrea@protonmail.ch>
2019-04-12 13:23:01 -0500

    Error handling

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,8 @@
 class Link < ApplicationRecord
-  belongs_to :user, optional: true # Prevent ActiveRecord::RecordInvalid
+  belongs_to :user, optional: true
+
+  validates :url, presence: true, length: { minimum: 5 }
+  validates :description, presence: true, length: { minimum: 5 }
 
   has_many :votes
 end
